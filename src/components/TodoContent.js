a574c0236bafe0a1f585db22a1b8df90da1db4b6
werachai <>
2023-12-26 20:23:58 +0700

    add feature update todo title

--- src/components/TodoContent.js
+++ src/components/TodoContent.js
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import { DELETE_TODO } from "../actions/todoAction";
 import axios from "../config/axios";
 import { TodoContext } from "../contexts/TodoContext";
-function TodoContent({ todo: { title, completed, id } }) {
+function TodoContent({ todo: { title, completed, id }, openEdit }) {
   const ctx = useContext(TodoContext);
   const handleClickDelete = () => {
     axios
@@ -14,7 +14,7 @@ function TodoContent({ todo: { title, completed, id } }) {
   };
   return (
     <div className="d-flex align-items-center">
-      <span className="flex-fill" role="button">
+      <span className="flex-fill" role="button" onClick={openEdit}>
         {title}
       </span>
       <div className="btn-group">
